ef5cf044b7549a63f720edacb353221f37368c93
frisk038 <frisktrentehuit@gmail.com>
2023-04-01 11:12:03 +0200

    return home

--- Script/game_control.gd
+++ Script/game_control.gd
@@ -71,3 +71,9 @@ func _ready():
 		print("cant connect new_path signal !", err)
 	updateCard()
 
+
+
+func _on_back_home_pressed():
+	var err = get_tree().change_scene("res://Scene/main_screen.tscn")
+	if err != OK :
+		print("cant load game scene")
